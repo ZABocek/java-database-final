22023fc9511e0cef8e07d69e0a652eee10cb58e9
ZABocek <60370860+ZABocek@users.noreply.github.com>
2026-03-07 15:42:52 +0100

    Refactor Review class with additional fields and methods

--- back-end/src/main/java/com/project/code/Model/Review.java
+++ back-end/src/main/java/com/project/code/Model/Review.java
@@ -1,85 +1,85 @@
 package com.project.code.Model;
 
-import jakarta.validation.constraints.NotNull;
-import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Id; 
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "reviews")
-public class Review {
-	@Id
-	private String id;
+import jakarta.validation.constraints.NotNull;
 
-	@NotNull(message = "Customer cannot be null")
-	private Long customerId;
-
-	@NotNull(message = "Product cannot be null")
-	private Long productId;
-
-	@NotNull(message = "Store cannot be null")
-	private Long storeId;
-
-	@NotNull(message = "Rating cannot be null")
-	private Integer rating;
-
-	private String comment;
-
-	public Review() {
-	}
-
-	public Review(Long customerId, Long productId, Long storeId, Integer rating, String comment) {
-		this.customerId = customerId;
-		this.productId = productId;
-		this.storeId = storeId;
-		this.rating = rating;
-		this.comment = comment;
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public Long getCustomerId() {
-		return customerId;
-	}
-
-	public void setCustomerId(Long customerId) {
-		this.customerId = customerId;
-	}
-
-	public Long getProductId() {
-		return productId;
-	}
-
-	public void setProductId(Long productId) {
-		this.productId = productId;
-	}
-
-	public Long getStoreId() {
-		return storeId;
-	}
-
-	public void setStoreId(Long storeId) {
-		this.storeId = storeId;
-	}
-
-	public Integer getRating() {
-		return rating;
-	}
-
-	public void setRating(Integer rating) {
-		this.rating = rating;
-	}
-
-	public String getComment() {
-		return comment;
-	}
-
-	public void setComment(String comment) {
-		this.comment = comment;
-	}
+@Document(collection = "reviews") 
+	public class Review {
+		@Id 
+		private String id;
+		
+		@NotNull(message = "Customer cannot be null") 
+		private Long customerId;
+		
+		@NotNull(message = "Product cannot be null") 
+		private Long productId; 
+		
+		@NotNull(message = "Store cannot be null") 
+		private Long storeId;
+		
+		@NotNull(message = "Rating cannot be null") 
+		private Integer rating;
+		
+		private String comment; 
+		
+		public Review() { }
+		
+		public Review(Long customerId, Long productId, Long storeId, Integer rating, String comment) { 
+			this.customerId = customerId; 
+			this.productId = productId; 
+			this.storeId = storeId; 
+			this.rating = rating; 
+			this.comment = comment; 
+		}
+		
+		public String getId() { 
+			return id; 
+		}
+		
+		public void setId(String id) { 
+			this.id = id; 
+		}
+		
+		public Long getCustomerId() { 
+			return customerId; 
+		}
+		
+		public void setCustomerId(Long customerId) { 
+			this.customerId = customerId; 
+		}
+		
+		public Long getProductId() { 
+			return productId; 
+		}
+		
+		public void setProductId(Long productId) { 
+			this.productId = productId; 
+		}
+		
+		public Long getStoreId() { 
+			return storeId; 
+		}
+		
+		public void setStoreId(Long storeId) { 
+			this.storeId = storeId; 
+		}
+		
+		public Integer getRating() { 
+			return rating; 
+		}
+		
+		public void setRating(Integer rating) { 
+			this.rating = rating; 
+		}
+		
+		public String getComment() { 
+			return comment; 
+		}
+		
+		public void setComment(String comment) { 
+			this.comment = comment; 
+		}
 
 }
