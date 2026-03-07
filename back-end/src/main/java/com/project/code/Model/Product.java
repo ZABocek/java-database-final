3418cd82f65f4a924b0aa91a642e2c60ed4af1f9
ZABocek <60370860+ZABocek@users.noreply.github.com>
2026-03-07 15:22:05 +0100

    Refactor Product class and add constructors
    Refactored Product class to improve structure and added constructors.

--- back-end/src/main/java/com/project/code/Model/Product.java
+++ back-end/src/main/java/com/project/code/Model/Product.java
@@ -1,89 +1,113 @@
 package com.project.code.Model;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
-import jakarta.persistence.UniqueConstraint;
-import jakarta.validation.constraints.NotNull;
-
-import java.util.ArrayList;
 import java.util.List;
 
-@Entity
-@Table(name = "product", uniqueConstraints = @UniqueConstraint(columnNames = "sku"))
-public class Product {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
-	@NotNull(message = "Name cannot be null")
-	private String name;
+import jakarta.persistence.Entity; 
+import jakarta.persistence.FetchType; 
+import jakarta.persistence.GeneratedValue; 
+import jakarta.persistence.GenerationType; 
+import jakarta.persistence.Id; 
+import jakarta.persistence.OneToMany; 
+import jakarta.persistence.Table; 
+import jakarta.persistence.UniqueConstraint; 
+import jakarta.validation.constraints.NotNull;
 
-	@NotNull(message = "Category cannot be null")
-	private String category;
-
-	@NotNull(message = "Price cannot be null")
-	private Double price;
-
-	@NotNull(message = "SKU cannot be null")
-	private String sku;
-
-	@OneToMany(mappedBy = "product")
-	@JsonManagedReference("inventory-product")
-	private List<Inventory> inventories = new ArrayList<>();
-
-	public Long getId() {
-		return id;
+@Entity 
+	@Table(name = "product", uniqueConstraints = @UniqueConstraint(columnNames = "sku")) 
+	public class Product { 
+		@Id 
+		@GeneratedValue(strategy = GenerationType.IDENTITY) 
+		private Long id;
+		@NotNull(message = "Name cannot be null") 
+		private String name;
+						  
+		@NotNull(message = "Name cannot be null") 
+		private String category;
+						  
+		@NotNull(message = "Name cannot be null") 
+		private Double price;
+						  
+		@NotNull(message = "Name cannot be null") 
+		private String sku;
+						  
+		@OneToMany(mappedBy = "product", fetch = FetchType.EAGER) 
+		@JsonManagedReference("inventory-product") 
+		private List inventory;
+						  
+		public Long getId() { 
+			return id; 
+		}
+						  
+		public void setId(Long id) { 
+			this.id = id; 
+		}
+						  
+		public String getName() { 
+			return name; 
+		}
+						  
+		public void setName(String name) { 
+			this.name = name; 
+		}
+						  
+		public String getCategory() { 
+			return category; 
+		}
+						  
+		public void setCategory(String category) { 
+			this.category = category; 
+		}
+						  
+		public Double getPrice() { 
+			return price; 
+		}
+						  
+		public void setPrice(Double price) { 
+			this.price = price; 
+		}
+						  
+		public String getSku() { 
+			return sku; 
+		}
+						  
+		public void setSku(String sku) { 
+			this.sku = sku; 
+		}
+						  
+		public List getInventory() { 
+			return inventory; 
+		}
+						  
+		public void setInventory(List inventory) { 
+			this.inventory = inventory; 
+		}
+						  
+		public Product() { }
+						  
+		public Product(String name, String category, Double price, String sku) { 
+			this.name = name; 
+			this.category = category; 
+			this.price = price; 
+			this.sku = sku; 
+		}
+						  
+		public String toString() { 
+			return "Product{" + 
+				"id=" + 
+				id + 
+				", name='" + 
+				name +
+				''' +
+				", category='" + 
+				category + 
+				''' + 
+				", price=" + 
+				price + 
+				", sku='" + 
+				sku + 
+				''' + 
+				'}'; 
+		} 
 	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getCategory() {
-		return category;
-	}
-
-	public void setCategory(String category) {
-		this.category = category;
-	}
-
-	public Double getPrice() {
-		return price;
-	}
-
-	public void setPrice(Double price) {
-		this.price = price;
-	}
-
-	public String getSku() {
-		return sku;
-	}
-
-	public void setSku(String sku) {
-		this.sku = sku;
-	}
-
-	public List<Inventory> getInventories() {
-		return inventories;
-	}
-
-	public void setInventories(List<Inventory> inventories) {
-		this.inventories = inventories;
-	}
-
-}
-
-
