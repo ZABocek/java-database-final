d016c05014684cdf9fd20bf416fa48d3eaf30798
ZABocek <60370860+ZABocek@users.noreply.github.com>
2026-03-07 15:52:25 +0100

    Fix formatting and reorder methods in CustomerRepository

--- back-end/src/main/java/com/project/code/Repo/CustomerRepository.java
+++ back-end/src/main/java/com/project/code/Repo/CustomerRepository.java
@@ -1,14 +1,9 @@
 package com.project.code.Repo;
 
-import com.project.code.Model.Customer;
+import com.project.code.Model.Customer; 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 	Customer findByEmail(String email);
-
-	Customer findByid(Long id);
+	Customer findByid(Long id); 
 }
-
-
