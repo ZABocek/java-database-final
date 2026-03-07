2f5a4e930ed3d4c21cf4974b317c551bbab334c2
ZABocek <60370860+ZABocek@users.noreply.github.com>
2026-03-07 11:19:37 +0100

    Add default constructor to Customer class

--- back-end/src/main/java/com/project/code/Model/Customer.java
+++ back-end/src/main/java/com/project/code/Model/Customer.java
@@ -71,6 +71,8 @@ public class Customer {
         this.orders = orders;
     }
 
+    public Customer() {}
+
     public Customer(String name, String email, String phone) { this.name = name; this.email = email; this.phone = phone; }
 
 }
