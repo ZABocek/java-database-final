4d4db3886314525527f34c4a0f5a60b5fe48e822
ZABocek <60370860+ZABocek@users.noreply.github.com>
2026-03-07 14:43:27 +0100

    Refactor InventoryRepository with proper annotations

--- back-end/src/main/java/com/project/code/Repo/InventoryRepository.java
+++ back-end/src/main/java/com/project/code/Repo/InventoryRepository.java
@@ -1,23 +1,24 @@
 package com.project.code.Repo;
 
-import com.project.code.Model.Inventory;
-import jakarta.transaction.Transactional;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
 import java.util.List;
 
-public interface InventoryRepository extends JpaRepository<Inventory, Long> {
-	@Query("SELECT i FROM Inventory i WHERE i.product.id = :productId AND i.store.id = :storeId")
-	Inventory findByProductIdandStoreId(@Param("productId") Long productId, @Param("storeId") Long storeId);
+import org.springframework.data.jpa.repository.JpaRepository; 
+import org.springframework.data.jpa.repository.Modifying; 
+import org.springframework.data.jpa.repository.Query; 
+import org.springframework.stereotype.Repository;
 
-	List<Inventory> findByStore_Id(Long storeId);
+import com.project.code.Model.Inventory;
 
-	@Modifying
-	@Transactional
-	@Query("DELETE FROM Inventory i WHERE i.product.id = :productId")
-	void deleteByProductId(@Param("productId") Long productId);
+import jakarta.transaction.Transactional;
+
+@Repository 
+	public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+		@Query("SELECT i FROM Inventory i WHERE i.product.id = :productId AND i.store.id = :storeId") 
+		Inventory findByProductIdandStoreId(Long productId, Long storeId);
+		List findByStore_Id(Long storeId);
+		@Modifying 
+		@Transactional 
+		@Query("DELETE FROM Inventory i WHERE i.product.id = :productId") 
+		void deleteByProductId(Long productId);
 
 }
