01ca8b5591e9c2289c2491c915671a796cc0aa21
ZABocek <60370860+ZABocek@users.noreply.github.com>
2026-03-07 11:33:51 +0100

    Refactor OrderDetails class and update annotations
    Refactored OrderDetails class to improve code structure and clarity.

--- back-end/src/main/java/com/project/code/Model/OrderDetails.java
+++ back-end/src/main/java/com/project/code/Model/OrderDetails.java
@@ -1,23 +1,20 @@
 package com.project.code.Model;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
-
-import java.time.LocalDateTime;
-import java.util.ArrayList;
+import java.time.LocalDateTime; 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import jakarta.persistence.Entity; 
+import jakarta.persistence.FetchType; 
+import jakarta.persistence.GeneratedValue; 
+import jakarta.persistence.GenerationType; 
+import jakarta.persistence.Id; 
+import jakarta.persistence.JoinColumn; 
+import jakarta.persistence.ManyToOne; 
+import jakarta.persistence.OneToMany;
+
 @Entity
-@Table(name = "order_details")
 public class OrderDetails {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,20 +22,21 @@ public class OrderDetails {
 
 	@ManyToOne
 	@JoinColumn(name = "customer_id")
-	@JsonBackReference("customer-orders")
+	@JsonBackReference
 	private Customer customer;
 
 	@ManyToOne
 	@JoinColumn(name = "store_id")
+	@JsonManagedReference
 	private Store store;
 
 	private Double totalPrice;
 
 	private LocalDateTime date;
 
-	@OneToMany(mappedBy = "order", fetch = FetchType.EAGER)
-	@JsonManagedReference("order-items")
-	private List<OrderItem> orderItems = new ArrayList<>();
+	@OneToMany(mappedBy = "order",fetch = FetchType.EAGER) 
+	@JsonManagedReference 
+	private List orderItems;
 
 	public OrderDetails() {
 	}
@@ -97,5 +95,11 @@ public class OrderDetails {
 	public void setOrderItems(List<OrderItem> orderItems) {
 		this.orderItems = orderItems;
 	}
+
+	public OrderDetails(Customer customer, Store store, Double totalPrice, LocalDateTime date) { 
+		this.customer = customer; 
+		this.store = store; 
+		this.totalPrice = totalPrice; 
+		this.date = date; } 
   
 }
