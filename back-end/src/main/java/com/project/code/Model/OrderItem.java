d0fc62dbfbe0be10d087c96b8c3fb43ff05c07a5
ZABocek <60370860+ZABocek@users.noreply.github.com>
2026-03-07 15:33:06 +0100

    Refactor OrderItem class and update JSON annotations

--- back-end/src/main/java/com/project/code/Model/OrderItem.java
+++ back-end/src/main/java/com/project/code/Model/OrderItem.java
@@ -1,83 +1,78 @@
 package com.project.code.Model;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import jakarta.persistence.Entity; 
+import jakarta.persistence.GeneratedValue; 
+import jakarta.persistence.GenerationType; 
+import jakarta.persistence.Id; 
+import jakarta.persistence.JoinColumn; 
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
 
-@Entity
-@Table(name = "order_item")
-public class OrderItem {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-
-	@ManyToOne
-	@JoinColumn(name = "order_id")
-	@JsonBackReference("order-items")
-	private OrderDetails order;
-
-	@ManyToOne
-	@JoinColumn(name = "product_id")
-	private Product product;
-
-	private Integer quantity;
-
-	private Double price;
-
-	public OrderItem() {
+@Entity 
+	public class OrderItem { 
+		@Id 
+		@GeneratedValue(strategy = GenerationType.IDENTITY) 
+		private Long id;
+		
+		@ManyToOne 
+		@JoinColumn(name = "order_id") 
+		@JsonManagedReference 
+		private OrderDetails order;
+		
+		@ManyToOne @JoinColumn(name = "product_id") 
+		@JsonManagedReference 
+		private Product product;
+		
+		private Integer quantity; 
+		private Double price;
+		
+		public Long getId() { 
+			return id; 
+		}
+		
+		public void setId(Long id) { 
+			this.id = id; 
+		}
+		
+		public OrderDetails getOrder() { 
+			return order; 
+		}
+		
+		public void setOrder(OrderDetails order) { 
+			this.order = order; 
+		}
+		
+		public Product getProduct() { 
+			return product; 
+		}
+		
+		public void setProduct(Product product) { 
+			this.product = product; 
+		}
+		
+		public Integer getQuantity() { 
+			return quantity; 
+		}
+		
+		public void setQuantity(Integer quantity) { 
+			this.quantity = quantity; 
+		}
+		
+		public Double getPrice() { 
+			return price; 
+		}
+		
+		public void setPrice(Double price) { 
+			this.price = price; 
+		}
+		
+		public OrderItem() {}
+		
+		public OrderItem(OrderDetails order, Product product, Integer quantity, Double price) { 
+			this.order = order; 
+			this.product = product; 
+			this.quantity = quantity; 
+			this.price = price; 
+		} 
 	}
-
-	public OrderItem(OrderDetails order, Product product, Integer quantity, Double price) {
-		this.order = order;
-		this.product = product;
-		this.quantity = quantity;
-		this.price = price;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public OrderDetails getOrder() {
-		return order;
-	}
-
-	public void setOrder(OrderDetails order) {
-		this.order = order;
-	}
-
-	public Product getProduct() {
-		return product;
-	}
-
-	public void setProduct(Product product) {
-		this.product = product;
-	}
-
-	public Integer getQuantity() {
-		return quantity;
-	}
-
-	public void setQuantity(Integer quantity) {
-		this.quantity = quantity;
-	}
-
-	public Double getPrice() {
-		return price;
-	}
-
-	public void setPrice(Double price) {
-		this.price = price;
-	}
-
-}
-
