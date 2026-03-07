a792efdb3a05f2f90f74a6ab2479c6eaef518537
ZABocek <60370860+ZABocek@users.noreply.github.com>
2026-03-07 15:49:38 +0100

    Refactor Store class and update inventory handling

--- back-end/src/main/java/com/project/code/Model/Store.java
+++ back-end/src/main/java/com/project/code/Model/Store.java
@@ -1,74 +1,72 @@
 package com.project.code.Model;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-
-import java.util.ArrayList;
 import java.util.List;
 
-@Entity
-public class Store {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
-	@NotNull(message = "Name cannot be null")
-	@NotBlank(message = "Name cannot be blank")
-	private String name;
+import jakarta.persistence.Entity; 
+import jakarta.persistence.FetchType; 
+import jakarta.persistence.GeneratedValue; 
+import jakarta.persistence.GenerationType; 
+import jakarta.persistence.Id; 
+import jakarta.persistence.OneToMany; 
+import jakarta.validation.constraints.NotBlank; 
+import jakarta.validation.constraints.NotNull;
 
-	@NotNull(message = "Address cannot be null")
-	@NotBlank(message = "Address cannot be blank")
-	private String address;
-
-	@OneToMany(mappedBy = "store")
-	@JsonManagedReference("inventory-store")
-	private List<Inventory> inventories = new ArrayList<>();
-
-	public Store() {
+@Entity 
+	public class Store { 
+		@Id 
+		@GeneratedValue(strategy = GenerationType.IDENTITY) 
+		private Long id;
+		
+		@NotNull(message = "Name cannot be null") 
+		@NotBlank(message = "Name cannot be blank") 
+		private String name; 
+		
+		@NotNull(message = "Address cannot be null") 
+		@NotBlank(message = "Address cannot be blank") 
+		private String address;
+		
+		@OneToMany(mappedBy = "store", fetch = FetchType.EAGER) 
+		@JsonManagedReference("inventory-store") 
+		private List inventory;
+		
+		public Long getId() { 
+			return id; 
+		}
+		
+		public void setId(Long id) { 
+			this.id = id; 
+		}
+		
+		public String getName() { 
+			return name; 
+		}
+		
+		public void setName(String name) { 
+			this.name = name; 
+		}
+		
+		public String getAddress() { 
+			return address; 
+		}
+		
+		public void setAddress(String address) { 
+			this.address = address; 
+		}
+		
+		public List getInventory() { 
+			return inventory; 
+		}
+		
+		public void setInventory(List inventory) { 
+			this.inventory = inventory; 
+		}
+		
+		public Store() { }
+		
+		public Store(String name, String address) { 
+			this.name = name; 
+			this.address = address; 
+		} 
 	}
-
-	public Store(String name, String address) {
-		this.name = name;
-		this.address = address;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	public List<Inventory> getInventories() {
-		return inventories;
-	}
-
-	public void setInventories(List<Inventory> inventories) {
-		this.inventories = inventories;
-	}
-
-}
-
