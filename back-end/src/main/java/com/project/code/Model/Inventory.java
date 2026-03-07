1aad78e4e9bd469c79eeeae6ad20e990eebd6ac8
ZABocek <60370860+ZABocek@users.noreply.github.com>
2026-03-07 15:01:19 +0100

    Refactor Inventory class with constructor and methods

--- back-end/src/main/java/com/project/code/Model/Inventory.java
+++ back-end/src/main/java/com/project/code/Model/Inventory.java
@@ -9,69 +9,70 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn; 
 import jakarta.persistence.ManyToOne;
 
-@Entity
-public class Inventory {
-   @Id
-   @GeneratedValue(strategy = GenerationType.IDENTITY)
-   private Long id;
+@Entity 
+   public class Inventory { 
+      @Id 
+      @GeneratedValue(strategy = GenerationType.IDENTITY) 
+      private Long id;
+      
+      @ManyToOne 
+      @JsonBackReference("inventory-product") 
+      @JoinColumn(name = "product_id") 
+      private Product product;
+      
+      @ManyToOne 
+      @JoinColumn(name = "store_id") 
+      @JsonBackReference("inventory-store") 
+      private Store store;
+      private Integer stockLevel;
+      
+      public Long getId() { 
+         return id; 
+      }
 
-   @ManyToOne
-   @JoinColumn(name = "product_id")
-   @JsonBackReference("inventory-product")
-   private Product product;
-
-   @ManyToOne
-   @JoinColumn(name = "store_id")
-   @JsonBackReference("inventory-store")
-   private Store store;
-
-   private Integer stockLevel;
-
-   public Inventory() {
+      public void setId(Long id) { 
+         this.id = id; 
+      }
+      
+      public Product getProduct() { 
+         return product; 
+      }
+      
+      public void setProduct(Product product) { 
+         this.product = product; 
+      }
+      
+      public Store getStore() { 
+         return store; 
+      }
+      
+      public void setStore(Store store) { 
+         this.store = store; 
+      }
+      
+      public Integer getStockLevel() { 
+         return stockLevel; 
+      }
+      
+      public void setStockLevel(Integer stockLevel) { 
+         this.stockLevel = stockLevel; 
+      }
+      
+      public Inventory() { }
+      
+      public Inventory(Product product, Store store, Integer stockLevel) { 
+         this.product = product; 
+         this.store = store; 
+         this.stockLevel = stockLevel; 
+      }
+      
+      public String toString() { 
+         return "Inventory{" + "id=" + id + ", product=" + 
+            (product != null ? product.getId() : "null") + 
+            ", store=" + 
+            (store != null ? store.getId() : "null") + 
+            ", stockLevel=" + 
+            stockLevel + 
+            '}'; 
+      } 
    }
-
-   public Inventory(Product product, Store store, Integer stockLevel) {
-      this.product = product;
-      this.store = store;
-      this.stockLevel = stockLevel;
-   }
-
-   public Long getId() {
-      return id;
-   }
-
-   public void setId(Long id) {
-      this.id = id;
-   }
-
-   public Product getProduct() {
-      return product;
-   }
-
-   public void setProduct(Product product) {
-      this.product = product;
-   }
-
-   public Store getStore() {
-      return store;
-   }
-
-   public void setStore(Store store) {
-      this.store = store;
-   }
-
-   public Integer getStockLevel() {
-      return stockLevel;
-   }
-
-   public void setStockLevel(Integer stockLevel) {
-      this.stockLevel = stockLevel;
-   }
-
-   public String toString() { 
-      return "Inventory{" + "id=" + id + ", product=" + 
-         (product != null ? product.getId() : "null") + ", store=" + 
-         (store != null ? store.getId() : "null") + ", stockLevel=" + stockLevel + '}'; } 
-
-}
-
